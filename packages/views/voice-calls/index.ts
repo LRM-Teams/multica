95230744a262168f784e27278b39c2fc9da9758f
le-czs <243511553+le-czs@users.noreply.github.com>
2026-07-23 23:06:11 +0800

    feat(voice-call): add browser RTC media adapter

---
+++ packages/views/voice-calls/index.ts
@@ -0,0 +1,8 @@
+export {
+  createVolcengineVoiceMediaSession,
+  VolcengineVoiceMediaSession,
+  VoiceCallMediaError,
+  type VoiceCallMediaErrorCode,
+  type VoiceCallMediaEvents,
+  type VoiceCallMediaState,
+} from "./volcengine-media-session";

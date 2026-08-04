92978f7ae3b9f3e104ec86c615f4eb22d8043fb1
le-czs <243511553+le-czs@users.noreply.github.com>
2026-08-04 15:54:57 +0800

    feat(research): add semantic aggregation projection

--- packages/core/research/index.ts
+++ packages/core/research/index.ts
@@ -2,6 +2,7 @@ export * from "./fleet-members";
 export * from "./mocks";
 export * from "./queries";
 export * from "./report-structured";
+export * from "./semantic-aggregation";
 export * from "./schemas";
 export * from "./ui-store";
 export * from "./ws-updaters";

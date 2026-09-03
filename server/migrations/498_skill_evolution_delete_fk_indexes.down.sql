4a683377202785970329f8e02508a9df9cbdb6e3
zhoujie22 <zhoujie22@lenovo.com>
2026-09-03 07:15:13 +0000

    fix(migrations): index skill evolution delete FKs

---
+++ server/migrations/498_skill_evolution_delete_fk_indexes.down.sql
@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS idx_skill_approval_evaluation;
+DROP INDEX IF EXISTS idx_skill_deployment_approval;
+DROP INDEX IF EXISTS idx_skill_deployment_target_channel_id;
+DROP INDEX IF EXISTS idx_skill_deployment_target_agent_id;
+DROP INDEX IF EXISTS idx_skill_evaluation_run_target_agent_id;
+DROP INDEX IF EXISTS idx_skill_evolution_run_target_agent_id;
